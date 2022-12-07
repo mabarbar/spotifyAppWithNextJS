cac456111e9bfac7c1513e12ab93c17201e5ef00
mabarbar <mateusz.barzenc00@gmail.com>
2022-12-07 22:00:06 +0100

    fixed added hosting and made app working

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['scdn.co','i.scdn.co', 'charts-images.scdn.co', 'seed-mix-image.spotifycdn.com', 'lineup-images.scdn.co', 'mosaic.scdn.co', 'thisis-images.scdn.co', 'rb.gy', 'daily-mix.scdn.co', 'newjams-images.scdn.co', 'seeded-session-images.scdn.co'],
+    domains: [
+      "scdn.co",
+      "i.scdn.co",
+      "charts-images.scdn.co",
+      "seed-mix-image.spotifycdn.com",
+      "lineup-images.scdn.co",
+      "mosaic.scdn.co",
+      "thisis-images.scdn.co",
+      "rb.gy",
+      "daily-mix.scdn.co",
+      "newjams-images.scdn.co",
+      "seeded-session-images.scdn.co",
+      "scontent-ams2-1.xx.fbcdn.net",
+    ],
   },
 };
